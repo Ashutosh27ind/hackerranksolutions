1264d6332a0f0046755c0c1a7ace86d8fef23990
Loneranger001 <asfkol@gmail.com>
2019-05-31 00:14:32 +0530

    saved

--- XMLParsing/Datacamp/parsingxml.py
+++ XMLParsing/Datacamp/parsingxml.py
@@ -7,19 +7,36 @@ tree = et.parse('movies.xml')
 # Get the root element
 root = tree.getroot()
 
-print(root.tag)
-print(root.attrib)
-
-for child in root:
-    print(child.tag, child.attrib)
+# print(root.tag)
+# print(root.attrib)
+#
+# for child in root:
+#     print(child.tag, child.attrib)
 # root.iter() : Itereates over all child elements & subchild
 
-all_tags = [elem.tag for elem in root.iter()]
-print(type(all_tags))
-print(len(all_tags))
+# all_tags = [elem.tag for elem in root.iter()]
+# print(type(all_tags))
+# print(len(all_tags))
 # commit
 # print(et.tostring(root, encoding='utf8').decode('utf8'))
-for movie in root.iter('movie'):
-    print(movie.attrib)
+# for movie in root.iter('movie'):
+#     print(movie.attrib)
 
+# print(et.tostring(root,encoding='utf8').decode('utf8'))
+#
+# for desc in root.iter('description'):
+#     print(desc.text)
 
+# Search by tag text
+#
+# for movie in root.findall("./genre/decade/movie/[rating = 'PG']"):
+#     print(movie.attrib['title'])
+#     print(movie.attrib['favorite'])
+#     # print(type(movie.attrib))
+
+# search by attribute
+
+# for movie in root.findall("./genre/decade/movie/[@favorite = 'True']"):
+#     print(movie.attrib['title'])
+#     print(movie.attrib['favorite'])
+#     # print(type(movie.attrib))
