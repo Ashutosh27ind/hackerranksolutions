91eb2122bb60d3072eaf9672c109d098b82a4a35
ALaskar <asfkol@gmail.com>
2019-05-30 20:00:47 +0530

    1. added comment at top. 2. added code to not process hidden sheets. 3. changed code to write exception in log file. 4. changed to remove invalid items from table.

--- XMLParsing/Datacamp/parsingxml.py
+++ XMLParsing/Datacamp/parsingxml.py
@@ -2,12 +2,24 @@ import os
 import datetime
 import xml.etree.ElementTree as et
 
+# Open the xml and get the root element
+tree = et.parse('movies.xml')
+# Get the root element
+root = tree.getroot()
 
+print(root.tag)
+print(root.attrib)
 
+for child in root:
+    print(child.tag, child.attrib)
+# root.iter() : Itereates over all child elements & subchild
 
+all_tags = [elem.tag for elem in root.iter()]
+print(type(all_tags))
+print(len(all_tags))
 
-
-
-
+# print(et.tostring(root, encoding='utf8').decode('utf8'))
+for movie in root.iter('movie'):
+    print(movie.attrib)
 
 
