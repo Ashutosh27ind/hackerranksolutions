eba2718094945d7b3d0b65497484b3153f5a7efa
Loneranger001 <asfkol@gmail.com>
2019-06-28 11:32:04 +0530

    new changes

--- XMLParsing/Datacamp/parsingxml.py
+++ XMLParsing/Datacamp/parsingxml.py
@@ -8,9 +8,18 @@ tree = et.parse('cd.xml')
 # Get the root element
 root = tree.getroot()
 
-
-
-
+# for cd in root.findall('CD'):
+#     # print('%s- %s- %s- %s- %s- %s' %(cd.find('TITLE').text,cd.find('ARTIST').text,cd.find('COUNTRY').text,cd.find('PRICE').text,cd.find('COUNTRY').text,
+#     #       cd.find('YEAR').text))
+#     list_of_values.append([cd.find('TITLE').text,cd.find('ARTIST').text,cd.find('COUNTRY').text,cd.find('PRICE').text,cd.find('COMPANY').text,
+#           cd.find('YEAR').text])
+#
+# with open('out.dat', 'w',newline='') as file:
+#     file_writer = csv.writer(file, delimiter='|', dialect='excel')
+#     file_writer.writerow(['TITLE','ARTIST','COUNTRY','PRICE','COMPANY','YEAR'])
+#     for line in list_of_values:
+#         file_writer.writerow(line)
+# file.close()
 # with open('out.txt', 'a',newline='') as file:
 #     file_writer = csv.writer(file, delimiter='|')
 #     for cd in root.findall('CD'):
